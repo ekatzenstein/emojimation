6c17d882eef8364f40664a7d5514cfaddeeb1509
Erick Katzenstein <erick.katzenstein@gmail.com>
2017-07-03 14:10:29 -0400

    test nwb

---
+++ demo/src/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+import LoadingButton from '../../src'
+
+class Demo extends Component {
+  state = { loading: false }
+
+  handleToggleLoading = () => {
+    this.setState({ loading: !this.state.loading })
+  }
+
+  render() {
+    return <div>
+      <h1>react-loading-button Demo</h1>
+
+      <h2>Static</h2>
+      <LoadingButton>Load</LoadingButton>
+      <LoadingButton loading>Loading</LoadingButton>
+
+      <h2>Dynamic</h2>
+      <LoadingButton loading={this.state.loading}>
+        {this.state.loading ? 'Loading' : 'Load'}
+      </LoadingButton>
+      <button type="button" onClick={this.handleToggleLoading}>
+        Toggle Loading
+      </button>
+    </div>
+  }
+}
+
+render(<Demo />, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import t from 'prop-types'
+import React, { Component } from 'react'
 
-import './index.css'
+class LoadingButton extends Component {
+  static propTypes = {
+    disabled: t.bool,
+    loading: t.bool,
+    type: t.string,
+  }
+  static defaultProps = {
+    disabled: false,
+    loading: false,
+    type: 'button',
+  }
+  render() {
+    let { children, disabled, loading, type, ...props } = this.props
+    if (loading) {
+      disabled = true
+    }
+    return <button disabled={disabled} type={type} {...props}>
+      {children}
+    </button>
+  }
+}
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+export default LoadingButton
